8d4586f44b7819259912d2b0e97e6e8fc5669c86
Dashawn Harry <Dashawnflint@gmail.com>
2023-03-02 20:41:55 -0600

    website rework with font change and animation added with images as  single carousel

--- client/src/components/HeroTop/Hero.jsx
+++ client/src/components/HeroTop/Hero.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-
-import Video1 from "../../personalimages/HeroVideo.mp4";
-import Video2 from "../../personalimages/IMG_2031.mp4";
-import Video3 from "../../personalimages/video2.mp4";
-
 import { Carousel } from "react-responsive-carousel";
+import { TypeAnimation } from "react-type-animation";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./hero.css";
@@ -21,13 +17,38 @@ const images = importAll(
   require.context("../../personalimages", false, /\.(png|jpe?g|svg)$/)
 );
 
-console.log(images)
+//14.png
+console.log(images);
 
 const Hero = () => {
+  const imageIndexArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
   return (
     <>
       <div className="heroContainer">
-        <div id="heroname">Harry Flint Flex Dashawn</div>
+        <div id="heroname">
+          <TypeAnimation
+            id="heroname"
+            sequence={[
+              "Harry",
+              2000,
+              "FlintFlex",
+              2000,
+              "Dashawn",
+              2000,
+              "Flint",
+            ]}
+            repeat={Infinity}
+            cursor={true}
+            style={{
+              color: "rgb(248, 252, 6)",
+              fontFamily: "Diesel",
+              fontSize: "10vw",
+              fontWeight: "500",
+            }}
+            speed={15}
+          />
+        </div>
       </div>
       <div className="wordcontainer">
         <Carousel
@@ -38,73 +59,14 @@ const Hero = () => {
           infiniteLoop={true}
           interval={10000}
         >
-          <div style={{ display: "flex", height: "100vh" }}>
-            <img className="image" src={images["0.png"].default} />
-
-            <img className="image" src={images["1.png"].default} />
-
-            <img className="image" src={images["2.png"].default} />
-          </div>
-          <div style={{ display: "flex", height: "100vh" }}>
-            <img className="image" src={images["3.png"].default} />
-
-            <img className="image" src={images["4.png"].default} />
-
-            <img className="image" src={images["5.png"].default} />
-          </div>
-          <div style={{ display: "flex", height: "100vh" }}>
-            <img className="image" src={images["6.png"].default} />
-
-            <img className="image" src={images["7.png"].default} />
-
-            <img className="image" src={images["8.png"].default} />
-          </div>
-          <div style={{ display: "flex", height: "100vh" }}>
-            <img className="image" src={images["9.png"].default} />
-
-            <img className="image" src={images["10.png"].default} />
-
-            <img className="image" src={images["11.png"].default} />
-          </div>
-          <div style={{ display: "flex", height: "100vh" }}>
-            <img className="image" src={images["12.png"].default} />
-
-            <img className="image" src={images["13.png"].default} />
-
-            <img className="image" src={images["14.png"].default} />
-          </div>
-          <div style={{ display: "flex", height: "100vh" }}>
-            <video
-              className="herovideo"
-              id="hero_video"
-              autoPlay
-              loop
-              muted
-              playsInline
-              src={Video1}
-              type="video/mp4"
-            />
-            <video
-              className="herovideo"
-              id="hero_video"
-              autoPlay
-              loop
-              muted
-              playsInline
-              src={Video2}
-              type="video/mp4"
-            />
-            <video
-              className="herovideo"
-              id="hero_video"
-              autoPlay
-              loop
-              muted
-              playsInline
-              src={Video3}
-              type="video/mp4"
-            />
-          </div>
+          {imageIndexArray.map((number, index) => {
+            console.log(index);
+            return (
+              <div style={{ display: "flex", height: "100vh" }}>
+                <img className="image" src={images[`${index}.png`].default} />
+              </div>
+            );
+          })}
         </Carousel>
       </div>
     </>
